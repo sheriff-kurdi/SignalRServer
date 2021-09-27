2bd826d56bcd2eaf074ff99f2180f241c9b5b0b2
sheriff.kurdi <sheriff.kurdi@gmail.com>
2021-09-27 16:38:18 +0200

    Add project files.

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message, string userId)
+        {
+            await Clients.User(userId).SendAsync("ReceiveMessage", $"this is private message from {user}: {message}");
+            //await Clients.All.SendAsync("ReceiveMessage", user, userId);
+        }
+    }
+}
